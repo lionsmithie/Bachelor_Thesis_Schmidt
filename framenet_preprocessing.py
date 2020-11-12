a5ee6f7f3a0b506dd11932fd9d991d5533bd8aac
lionsmithie <lschmidt@cl.uni-heidelberg.de>
2020-11-12 17:41:30 +0100

    Testing

--- framenet_preprocessing.py
+++ framenet_preprocessing.py
@@ -1,12 +1,12 @@
 from nltk.corpus import framenet as fn
 
-lu_cook = fn.lus(r'.*cook\.v')
+#lu_cook = fn.lus(r'.*cook\.v')
 
 #print(lu_cook)  #returns a list of all lexical units which evoke a frame. (Can be more than one)
 #print(lu_cook[2])  #refers to one lexical unit and its respective frame.
 
-cook_sense3 = lu_cook[2]
+#cook_sense3 = lu_cook[2]
 
-print(cook_sense3.keys())  # to see what attributes / keys are saved for this object.
+#print(cook_sense3.keys())  # to see what attributes / keys are saved for this object.
 #print(cook_sense3.frame)   # getting the info about the frame.
 #print(cook_sense3.frame.name)   # refers to the frame object, same procedure as for a lexical unit.
